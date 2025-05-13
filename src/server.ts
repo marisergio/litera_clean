f1e683089b42e8d8f7fb91e8cdaed1e49ef332eb
marisergio <marisergio@gmail.com>
2025-05-13 19:03:30 -0300

    create function main

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
-import express from 'express';
 import LivroRoutes from './infra/web/express/routes/LivroRoutes';
 import { ApiExpress } from './infra/web/express/ApiExpress';
 
-const app = new ApiExpress(LivroRoutes);
-app.start(3000);
+function main() {
+    const app = new ApiExpress(LivroRoutes);
+    app.start(3000);
+}
+
+
+main();
